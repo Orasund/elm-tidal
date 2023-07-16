80fcfbe86c90dd3eac6fd7541f387be325035f74
Lucas Payr <orasund@gmail.com>
2023-07-16 19:59:28 +0200

    add TidalCycle

--- src/Tidal.elm
+++ src/Tidal.elm
@@ -1,4 +1,4 @@
-module Tidal exposing (..)
+module Tidal exposing (TidalCycle, note, sound, toHtml, toString)
 
 import Html exposing (Attribute, Html)
 import Html.Attributes
@@ -6,16 +6,40 @@ import Tidal.Mini exposing (TidalPattern)
 import Url
 
 
-mini : TidalPattern -> String
-mini =
-    Tidal.Mini.toString
+type TidalCycle
+    = Note TidalPattern
+    | Sound TidalPattern TidalCycle
 
 
-toHtml : List (Attribute msg) -> String -> Html msg
-toHtml attrs string =
+note : TidalPattern -> TidalCycle
+note =
+    Note
+
+
+sound : TidalPattern -> TidalCycle -> TidalCycle
+sound =
+    Sound
+
+
+toString : TidalCycle -> String
+toString tidalcycle =
+    case tidalcycle of
+        Note pattern ->
+            "note(\"" ++ Tidal.Mini.toString pattern ++ "\")"
+
+        Sound pattern cycle ->
+            (toString cycle ++ "\n")
+                ++ "  "
+                ++ (".sound(\"" ++ Tidal.Mini.toString pattern ++ "\")")
+
+
+toHtml : List (Attribute msg) -> TidalCycle -> Html msg
+toHtml attrs tidalcycles =
     let
         urlComponent =
-            Url.percentEncode string
+            tidalcycles
+                |> toString
+                |> Url.percentEncode
     in
     Html.iframe
         (("https://strudel.tidalcycles.org/#" ++ urlComponent |> Html.Attributes.src)
